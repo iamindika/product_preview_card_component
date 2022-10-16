e160e291a401c7a8f3e5153aa48b610742cae573
iamindika <ikolamba2@gmail.com>
2022-10-15 23:21:42 -0400

    render product list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import ProductList from "./components/ProductList";
 import data from "./db/data";
 
 function App() {
   return (
-    <div>
-      {JSON.stringify(data, null, 2)}
-    </div>
+    <main>
+      <ProductList inventory={data} />
+    </main>
   )
 }
 
