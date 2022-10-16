f96932c288982f3645eca410cc7ac9b03a43cdbc
iamindika <ikolamba2@gmail.com>
2022-10-15 21:12:04 -0400

    add more data entries

--- src/db/data.js
+++ src/db/data.js
@@ -9,8 +9,65 @@ export default [
       "sale": "149.99"
     },
     "image": {
-      "desktop": "image-product-desktop.jpg",
-      "mobile": "image-product-mobile.jpg"
+      "desktop": "olena-sergienko-GOVTETevRm8-unsplash-desktop.jpg",
+      "mobile": "olena-sergienko-GOVTETevRm8-unsplash-mobile.jpg"
     }
-  }
+  },
+  { 
+    "id": 2,
+    "category": "Perfume",
+    "title": "Coco Noir Eau De Parfum",
+    "description": "Intimate, seductive and intensely brilliant, COCO NOIR is a contemporary expression of magnetic sensuality told in a modern ambery fragrance.",
+    "price": {
+      "original": "193.00",
+      "sale": ""
+    },
+    "image": {
+      "desktop": "laura-chouette-4sKdeIMiFEI-unsplash-desktop.jpg",
+      "mobile": "laura-chouette-4sKdeIMiFEI-unsplash-mobile.jpg"
+    }
+  },
+  { 
+    "id": 3,
+    "category": "Perfume",
+    "title": "Gentleman Givenchy Eau De Parfum",
+    "description": "An explosive fragrance clashing powerful iris with spicy black pepper and deep vanilla wood.",
+    "price": {
+      "original": "152.00",
+      "sale": ""
+    },
+    "image": {
+      "desktop": "alina-rubo-HjO5vw_PP5c-unsplash-desktop.jpg",
+      "mobile": "alina-rubo-HjO5vw_PP5c-unsplash-mobile.jpg"
+    }
+  },
+  { 
+    "id": 4,
+    "category": "Perfume",
+    "title": "Sun Di Gioia Eau De Parfum",
+    "description": "Inspired by the breeze and the summer sun. Crisp, sweet, sunny, creamy, warm and enlivening. Top notes of bergamot and freesia by Giorgio Armani.",
+    "price": {
+      "original": "179.00",
+      "sale": "155.00"
+
+    },
+    "image": {
+      "desktop": "karly-jones-4i9ef6xU738-unsplash-desktop.jpg",
+      "mobile": "karly-jones-4i9ef6xU738-unsplash-mobile.jpg"
+    }
+  },
+  { 
+    "id": 5,
+    "category": "Perfume",
+    "title": "The One Eau De Parfum",
+    "description": "Dolce & Gabbana The One for Men is an elegant, sensual perfume that is decidedly modern but also a unique, timeless classic.",
+    "price": {
+      "original": "154.99",
+      "sale": "119.99"
+    },
+    "image": {
+      "desktop": "mitra-jml-vPVBZxfyHOQ-unsplash-desktop.jpg",
+      "mobile": "mitra-jml-vPVBZxfyHOQ-unsplash-mobile.jpg"
+    }
+  },
 ];
