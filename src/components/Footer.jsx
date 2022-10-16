a78aab7c394344ff8e8d4299eb97872a6e3beeb9
iamindika <ikolamba2@gmail.com>
2022-10-16 16:01:32 -0400

    create footer component and footer styles
    add frontend-mentor attribution

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => (
+  <footer className="footer">
+    <p className="footer-text">
+      Challenge by{' '}
+      <a 
+        className="footer-link"
+        href="https://www.frontendmentor.io/challenges/product-preview-card-component-GO7UmttRfa"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Frontend Mentor</a>.{' '} 
+      Coded by{' '}
+      <a 
+        className="footer-link"
+        href="https://www.frontendmentor.io/profile/iamindika"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Indika KT</a>.
+    </p>
+  </footer>
+);
+
+export default Footer;
